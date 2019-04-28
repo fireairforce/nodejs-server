542aea6d1b0e9fbc71a1c4f58e4aebdf965b28a7
zoomdong <1344492820@qq.com>
2019-04-28 12:03:32 +0800

    feat: 完成模板引擎的渲染

--- src/helper/route.js
+++ src/helper/route.js
@@ -2,6 +2,16 @@ const fs = require('fs') // 用于判断模块是文还是文件夹
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir); // 让一些异步操作能够写成同步的形式
+const HandleBars = require('handlebars');
+const path = require('path');
+const config = require('../config/defaultConfig')
+const chalk = require('chalk')
+
+
+const tplPath = path.join(__dirname,'../template/dir.tpl.html')
+const source = fs.readFileSync(tplPath); 
+// fs会读出来一个buffer文件流，这里在传入模板引擎去进行编译的时候修改为字符串类型
+const template = HandleBars.compile(source.toString()) 
 
 module.exports = async function(req,res,filePath){
     try {
@@ -13,8 +23,15 @@ module.exports = async function(req,res,filePath){
           } else if(stats.isDirectory()) {
               const files = await readdir(filePath); // 这里一定要加上await的关键字
               res.statusCode = 200;
-              res.setHeader('Content-Type','text/plain');
-              res.end(files.join(','))  
+              res.setHeader('Content-Type','text/html');
+              const dir = path.relative(config.root,filePath)
+              console.log(chalk.green(dir))
+              const data = {
+                  titles:path.basename(filePath),
+                  dir: dir? `/${dir}` : '',
+                  files
+              };
+              res.end(template(data))  
           }
       } catch(ex) {
           console.log(ex)
