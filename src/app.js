6fd16018016fc3222cacb94b555f8f3d5686df01
zoomdong <1344492820@qq.com>
2019-04-28 19:31:29 +0800

    feat: open automatically

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const conf = require('./config/defaultConfig')
 const chalk = require('chalk');
 const path = require('path');
 const route = require('./helper/route')
+const openUrl = require('./helper/openUrl')
 class Server {
   constructor(config){
     this.conf = Object.assign({},conf,config) // 对配置文件进行merge
@@ -16,6 +17,7 @@ class Server {
     server.listen(this.conf.port,this.conf.hostname,()=>{
         const addr = `http://${this.conf.hostname}:${this.conf.port}`;
         console.info(`Server stared at ${chalk.green(addr)}`);
+        openUrl(addr) // 让他帮我们自动打开
     });
   }
 }
